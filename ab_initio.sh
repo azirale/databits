b3fd9298539f64ded515b1f82ac76477ab9829a0
Jon <azirale@gmail.com>
2025-07-12 09:42:30 +1000

    update python libs in use in samples

--- ab_initio.sh
+++ ab_initio.sh
@@ -1,11 +1,14 @@
-# this is for linux/wsl
+# if you are on windows use wsl2, it makes all data work significantly easier
 # run these commands yourself to get started
 
-# install homebrew - this helps install other items
+# install homebrew (https://brew.sh/) - this helps install other items
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install uv - used to manage python venv and lib installs
 brew install uv
 
-# get uv to install python and libs
+# install ruff - very nice for python formatting
+brew install ruff
+
+# get uv to install python and libs based on pyproject.toml
 uv sync
